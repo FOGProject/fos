0b4963d9e619c5bf19fd3ea26d46d1e3b3c95d7b
Tom Elliott <tommygunsster@gmail.com>
2018-02-10 00:55:48 -0500

    Update docker file to include the libelf-dev and libelf-dev:i386 for the image. (Needs repo updated too, mastacontrola/fos-builder is available with this modified docker image now.) Update build.sh script to build 32 bit inits with the ARCH=i486 flag. Otherwise it's building against 64 bit code which would, more or less, make 32 bit init's unusable. Will monitor to ensure all builds appropriately however.

--- build.sh
+++ build.sh
@@ -185,23 +185,28 @@ function buildFilesystem() {
     if [[ $confirm != n ]]; then
         read -p "We are ready to build. Would you like to edit the config file [y|n]?" config
         if [[ $config == y ]]; then
-            make menuconfig
+            [[ $arch == x64 ]] && make menuconfig || make ARCH=i486 menuconfig
         else
             echo "Ok, running make oldconfig instead to ensure the config is clean."
-            make oldconfig
+            [[ $arch == x64 ]] && make oldconfig || make ARCH=i486 oldconfig
         fi
         read -p "We are ready to build are you [y|n]?" ready
         if [[ $ready == y ]]; then
             echo "This make take a long time. Get some coffee, you'll be here a while!"
-            make -j $(nproc) >buildroot$arch.log
+            [[ $arch == x64 ]] && make -j $(nproc) >buildroot$arch.log || make ARCH=i486 -j $(nproc) >buildroot$arch.log
         else
             echo "Nothing to build!? Skipping."
 	    cd ..
             return
         fi
     else
-        make oldconfig
-        make -j $(nproc) >buildroot$arch.log
+        [[ $arch == x64 ]] && {
+            make oldconfig
+            make -j $(nproc) >buildroot$arch.log
+        } || {
+            make ARCH=i486 oldconfig
+            make ARCH=i486 -j $(nproc) >buildroot$arch.log
+        }
     fi
     cd ..
     kill $PING_LOOP_PID
@@ -251,6 +256,7 @@ function buildKernel() {
             git clone git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git >/dev/null 2>&1
             echo "Done"
         fi
+        make mrproper
     fi
     if [[ -f .config ]]; then
         echo "Configuration kernelsource$arch/.config already exists, will reuse it."
