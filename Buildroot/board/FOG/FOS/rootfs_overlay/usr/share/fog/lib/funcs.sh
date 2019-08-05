fd4c41a56dd13d27c4a4f91ce4c6ccb16dbfb356
Quazz <nickverschragen@gmail.com>
2019-08-05 11:07:49 +0200

    Fix invalid partclone parameter

--- Buildroot/board/FOG/FOS/rootfs_overlay/usr/share/fog/lib/funcs.sh
+++ Buildroot/board/FOG/FOS/rootfs_overlay/usr/share/fog/lib/funcs.sh
@@ -2050,7 +2050,7 @@ savePartition() {
                     debugPause
                     imgpart="$imagePath/d${disk_number}p${part_number}.img"
                     uploadFormat "$fifoname" "$imgpart"
-                    partclone.$fstype -aX0 -n "Storage Location $storage, Image name $img" -cs $part -O $fifoname -Nf 1
+                    partclone.$fstype -a0 -n "Storage Location $storage, Image name $img" -cs $part -O $fifoname -Nf 1
                     exitcode=$?
                     case $exitcode in
                         0)
