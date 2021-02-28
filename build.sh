dd0b8c98d950de03d6a346d9006704e139682d48
Sebastian Roth <sebaroth@gmx.de>
2021-02-28 14:21:54 -0600

    Update to Buildroot version 2020.02.11 and add gptfdisk package hash to fix compile error

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 [[ -z $KERNEL_VERSION ]] && KERNEL_VERSION='5.10.19'
-[[ -z $BUILDROOT_VERSION ]] && BUILDROOT_VERSION='2020.02.6'
+[[ -z $BUILDROOT_VERSION ]] && BUILDROOT_VERSION='2020.02.11'
 
 Usage() {
     echo -e "Usage: $0 [-knfvh?] [-a x64]"
