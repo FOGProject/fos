49cf750c8c1d199e91d7ca191071050582d22772
Sebastian Roth <sebaroth@gmx.de>
2021-07-25 21:47:55 +0200

    Add essential checks and variables to release script

--- release.sh
+++ release.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-buildkite-agent artifact download dist/* .
-ls -alR dist/
-ls -al /var/lib/buildkite-agent/
-ls -al /var/lib/buildkite-agent/builds/
-ls -al /var/lib/buildkite-agent/builds/Tollana-1/
+. /var/lib/buildkite-agent/github-upload.sh
+[[ -z ${GITHUB_USER} -o -z ${GITHUB_TOKEN} ]] && echo "Missing Github information, can't proceed" && exit 1
+
+command -v jqa
+[[ $? -eq 0 ]] || echo "Package jq not installed, can't proceed" && exit 1
+
+GITHUB_TAG=$(date +%Y%m%d)
+GITHUB_NAME="Latest from $(date +%d.%m.%Y)"
+
+echo "Today is ${today}"
+
+#buildkite-agent artifact download dist/* .
+#cd dist/
+
+#curl -X POST -u ${GITHUB_USER}:${GITHUB_TOKEN} -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/FOGProject/fos/releases -d "{ \"tag_name\":\"${GITHUB_TAG}\", \"name\":\"${GITHUB_NAME}\" }"
