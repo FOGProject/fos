08a6581413a19c457babfe35d2190834a273b7a1
Sebastian Roth <sebaroth@gmx.de>
2022-02-12 20:21:20 +0100

    Add testing stage to the release script to update tag and release instead of creating a new one on github.

--- release.sh
+++ release.sh
@@ -8,19 +8,28 @@ command -v curl
 command -v jq
 [[ $? -ne 0 ]] && echo "Package jq not installed, can't proceed." && exit 1
 
-GITHUB_TAG=$(date +%Y%m%d)
-GITHUB_NAME="Latest from $(date +%d.%m.%Y)"
-
 KERNEL_VERSION=$(grep KERNEL_VERSION= build.sh | cut -d"'" -f2)
 BUILDROOT_VERSION=$(grep BUILDROOT_VERSION= build.sh | cut -d"'" -f2)
 
-curl -s -X POST -u ${GITHUB_USER}:${GITHUB_TOKEN} -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/FOGProject/fos/releases -d "{ \"tag_name\":\"${GITHUB_TAG}\", \"name\":\"${GITHUB_NAME}\", \"body\":\"Linux kernel ${KERNEL_VERSION}\nBuildroot ${BUILDROOT_VERSION}\" }" > create_release_response.json
+if [[ -n "$1" ]]; then
+    GITHUB_TAG=testing
+    GITHUB_NAME="Testing from $(date +%d.%m.%Y)"
+    TESTING_RELEASE_ID=$(curl -s -X GET -u ${GITHUB_USER}:${GITHUB_TOKEN} -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/FOGProject/fos/releases/tags/${GITHUB_TAG} | jq -r .id)
+    HEAD_SHA=$(curl -s -X GET -u ${GITHUB_USER}:${GITHUB_TOKEN} -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/FOGProject/fos/git/refs/heads/${1} | jq -r .object.sha)
+    curl -s -X PATCH -u ${GITHUB_USER}:${GITHUB_TOKEN} -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/FOGProject/fos/git/refs/${GITHUB_TAG} -d "{ \"sha\":\"${HEAD_SHA}\" }" > tag_update_response.json
+    curl -s -X PATCH -u ${GITHUB_USER}:${GITHUB_TOKEN} -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/FOGProject/fos/releases/${TESTING_RELEASE_ID} -d "{ \"tag_name\":\"${GITHUB_TAG}\", \"name\":\"${GITHUB_NAME}\", \"body\":\"Linux kernel ${KERNEL_VERSION}\nBuildroot ${BUILDROOT_VERSION}\" } > response.json
+else
+    GITHUB_TAG=$(date +%Y%m%d)
+    GITHUB_NAME="Latest from $(date +%d.%m.%Y)"
+    curl -s -X POST -u ${GITHUB_USER}:${GITHUB_TOKEN} -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/FOGProject/fos/releases -d "{ \"tag_name\":\"${GITHUB_TAG}\", \"name\":\"${GITHUB_NAME}\", \"body\":\"Linux kernel ${KERNEL_VERSION}\nBuildroot ${BUILDROOT_VERSION}\" }" > response.json
+fi
 
-GITHUB_RELEASE_ID=$(cat create_release_response.json | jq -r .id)
 
-[[ -z ${GITHUB_RELEASE_ID} || ${GITHUB_RELEASE_ID} == "null" ]] && echo "ID not found in response, something went wrong when trying to create a release on Github." && cat create_release_response.json && exit 1
+GITHUB_RELEASE_ID=$(cat response.json | jq -r .id)
 
-echo "New release created on Github, tagged ${GITHUB_TAG}, id ${GITHUB_RELEASE_ID}."
+[[ -z ${GITHUB_RELEASE_ID} || ${GITHUB_RELEASE_ID} == "null" ]] && echo "ID not found in response, something went wrong when trying to create or update the release on Github." && cat response.json && exit 1
+
+echo "Release created/updated on Github, tagged ${GITHUB_TAG}, id ${GITHUB_RELEASE_ID}."
 
 buildkite-agent artifact download 'dist/*' .
 cd dist/
