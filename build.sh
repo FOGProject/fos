29e7b7dd697b4428e1ef34f5b7c1fb76d78cbc36
Sebastian Roth <sebaroth@gmx.de>
2022-04-16 10:28:22 -0500

    Update Linux kernel to version 5.15.34

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[[ -z $KERNEL_VERSION ]] && KERNEL_VERSION='5.15.19'
+[[ -z $KERNEL_VERSION ]] && KERNEL_VERSION='5.15.34'
 [[ -z $BUILDROOT_VERSION ]] && BUILDROOT_VERSION='2021.02.8'
 
 Usage() {
