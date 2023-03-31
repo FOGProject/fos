1dfc29dc59f9b2af379c897f7517a96861609e26
Sebastian Roth <sebaroth@gmx.de>
2023-03-31 00:02:46 +0000

    Update Linux kernel to 6.1.22

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[[ -z $KERNEL_VERSION ]] && KERNEL_VERSION='5.15.98'
+[[ -z $KERNEL_VERSION ]] && KERNEL_VERSION='6.1.22'
 [[ -z $BUILDROOT_VERSION ]] && BUILDROOT_VERSION='2022.02.9'
 
 Usage() {
