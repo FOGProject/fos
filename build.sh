ae684d9e2592760df137b205ac7e684c3fd50a43
Tom Elliott <tommygunsster@gmail.com>
2024-07-02 10:00:30 -0500

    Should revert to LTS of buildroot and update configs to use 6.6 headers

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 [[ -z $KERNEL_VERSION ]] && KERNEL_VERSION='6.6.34'
-[[ -z $BUILDROOT_VERSION ]] && BUILDROOT_VERSION='2024.05'
+[[ -z $BUILDROOT_VERSION ]] && BUILDROOT_VERSION='2024.02.3'
 
 Usage() {
     echo -e "Usage: $0 [-knfvh?] [-a x64]"
