c69a804e3aeafcef53a823905cc9883a61cd493f
rluzuriaga <rodrigoluzuriaga@icloud.com>
2025-07-01 22:35:27 -0700

    chore(configs): update buildroot to 2025.02.4

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 source ./dependencies.sh
 
 [[ -z $KERNEL_VERSION ]] && KERNEL_VERSION='6.12.35'
-[[ -z $BUILDROOT_VERSION ]] && BUILDROOT_VERSION='2025.02'
+[[ -z $BUILDROOT_VERSION ]] && BUILDROOT_VERSION='2025.02.4'
 
 declare -ar ARCHITECTURES=("x64" "x86" "arm64")
 PIPE_JOINED_ARCHITECTURES=$(IFS="|"; echo "${ARCHITECTURES[@]}"; unset IFS)
